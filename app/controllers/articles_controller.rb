85e6b3414b53ec225bfaf81b37f83ea0f0093386
abhishekgarfield <abhishek2759@gmail.com>
2022-12-03 13:14:13 +0530

    completeded

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,51 @@
 class ArticlesController < ApplicationController
+
+    def index
+        @articles=Article.all
+    end
+
+    def show
+        @article=Article.find(params[:id])
+    end
+
+    def edit 
+        @article=Article.find(params[:id])
+    end
+
+    def new
+        @article=Article.new
+    end
+
+    def create
+        @article=Article.new(check_params)
+        if(@article.save)
+            redirect_to article_path(@article)
+        else
+            render :new , status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @article=Article.find(params[:id])
+        puts "@@@@@#{@article[:title]}"
+        if(@article.update(check_params))
+            redirect_to @article
+        else
+            render :edit , status: :unprocessable_entity
+        end
+    end
+
+    def destroy 
+        @article=Article.find(params[:id])
+        @article.delete
+        redirect_to "/"
+    end
+
+
+    private
+
+    def check_params
+        params.require(:article).permit(:title,:body)
+    end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    validates :title , presence: true , length: {minimum: 8 , message: "Length should be >= 8"}
+    validates :body , presence:true
 end
