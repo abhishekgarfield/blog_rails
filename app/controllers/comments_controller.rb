6aab3a024004a7b628c930095236069ca4ec2b4c
abhishekgarfield <abhishek2759@gmail.com>
2022-12-04 07:38:00 +0530

    comments error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,18 @@
 class CommentsController < ApplicationController
+    def new 
+        @comment=Comment.new
+    end
     def create
         @article=Article.find(params[:article_id])
-        @comment=@article.comments.create(check_params)
-        redirect_to article_path(@article)
+        @comment=@article.comments.new(check_params)
+        if @article.save
+            redirect_to article_path(@article)
+        else
+            render :"articles/show" , status: :unprocessable_entity
+        end
     end
+    
+
     private
     def check_params
         params.require(:comment).permit(:commenter,:body)
