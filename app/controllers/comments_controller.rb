eb98419f88ea6e8afad2d38016ebf87cc11566d0
abhishekgarfield <abhishek2759@gmail.com>
2022-12-04 23:27:00 +0530

    hj

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,10 @@ class CommentsController < ApplicationController
         @comment=Comment.new
     end
     def create
+        puts "#{params}"
         @article=Article.find(params[:article_id])
         @comment=@article.comments.new(check_params)
+        puts "#{@comment[:article_id]}"
         if @article.save
             redirect_to article_path(@article)
         else
